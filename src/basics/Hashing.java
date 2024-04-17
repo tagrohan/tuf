68671baf58c4a890a5d7b442ea2a90e16191df14
tagrohan <tagrohan@gmail.com>
2024-04-17 10:12:51 +0530

    print highest and lowest in array, using Pair class

--- src/basics/Hashing.java
+++ src/basics/Hashing.java
@@ -22,5 +22,49 @@ public class Hashing {
         map.forEach((k, v) -> {
             System.out.println(k + " -> " + v);
         });
+
+        // highest and lowest frequesncy element
+        // here we need to keep the recors of both key and frequesncy so that we can compare and changes based on that
+        Pair min = new Pair(0, Integer.MAX_VALUE);
+        Pair max = new Pair(0, Integer.MIN_VALUE);
+
+        for (Integer i : map.keySet()) {
+            if (map.get(i) > max.getFreq()) {
+                max.setKey(i);
+                max.setFreq(map.get(i));
+            }
+            if (map.get(i) < min.getFreq()) {
+                min.setKey(i);
+                min.setFreq(map.get(i));
+            }
+
+        }
+        System.out.println("Max is " + max.getKey() + ", Min is " + min.getKey());
+    }
+
+    static class Pair {
+        int key;
+        int freq;
+
+        public Pair(int key, int freq) {
+            this.key = key;
+            this.freq = freq;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setKey(int key) {
+            this.key = key;
+        }
+
+        public int getFreq() {
+            return freq;
+        }
+
+        public void setFreq(int freq) {
+            this.freq = freq;
+        }
     }
 }
