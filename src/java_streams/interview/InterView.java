a224f6412e8a1d89276262f35ae7f01806dca3d2
tagrohan <tagrohan@gmail.com>
2025-04-10 22:21:35 +0530

    refreshing

--- src/java_streams/interview/InterView.java
+++ src/java_streams/interview/InterView.java
@@ -1,14 +1,14 @@
 package java_streams.interview;
 
-import java.util.*;
-import java.util.stream.Collector;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InterView {
 
     public static void main(String[] args) {
-        sortArray();
+//        sortArray();
 
     }
 
